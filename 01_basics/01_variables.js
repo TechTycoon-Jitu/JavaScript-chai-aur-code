a72eb7ed8f70bd64a72401ec5428337a89a76e14
Sanket Mondal <112824335+TechTycoon-Jitu@users.noreply.github.com>
2024-01-28 16:26:31 +0000

    variable

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -15,4 +15,4 @@ console.log(accountId);
  Prefer not to use var 
  because of issue in block scope and functional scope
 */
-console.table([accountId,accountEmail,accountPassword,accountCity,accountState]) 
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
