4333f6059a314eeb6d7f2e3b0341db14cbf3d73f
Sanket Mondal <112824335+TechTycoon-Jitu@users.noreply.github.com>
2024-02-26 13:28:27 +0000

    objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,35 @@
+//const tinderuser = new object() //This is singleton object
+const tinderuser = {} //This is non singleton object
+tinderuser.id = "123abc"
+tinderuser.name = "Sammy"
+tinderuser.isLoggedIn = false
+// console.log(tinderuser);
+
+const regularUser = {
+    email:"some@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"Sanket",
+            lastname:"Mondal"
+        }
+    }
+}
+//console.log(regularUser.fullname.userfullname.firstname);
+
+
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"a",4:"b"}
+const obj4 = {5:"a",6:"b"}
+
+//const obj3 = {obj1,obj2}
+//const obj3 = Object.assign({},obj1,obj2,obj4)
+const obj3 = {...obj1,...obj2};
+// console.log(obj3);
+
+console.log(Object.keys(tinderuser));
+console.log(Object.values(tinderuser));
+console.log(Object.entries(tinderuser));
+
+console.log(tinderuser.hasOwnProperty('isLogged'));
+
+
